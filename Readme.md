ccf2753940ea0ff787120d09f7f4b481aef763bf
luccasrolland <luccas.rolland@gmail.com>
2022-08-23 12:55:27 -0300

    Criando aplicação de consumo de API pública

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# meu-buscador-de-cep
+
+ # Consumindo uma API pública.
+
+- Classe aonde ficará os dados que queremos.
+- Método aonde pegamos o CEP do usuário.
+- Status de requisição da resposta.
+- Método aonde nos retorna o JSON de resposta.
+- Conversão de JSON para String.
+- Atribuição dos dados em JSON em um objeto.
+- Utilização de uma biblioteca do Google chamada GSON.
---
+++ src/meubuscadordecep/cliente/Main.java
@@ -0,0 +1,21 @@
+package meubuscadordecep.cliente;
+
+import meubuscadordecep.dominio.Endereco;
+import meubuscadordecep.viacep.ServicoDeCep;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.print("Informe seu CEP: ");
+        String cep = new Scanner(System.in).nextLine();
+        Endereco endereco =  ServicoDeCep.buscaEnderecoPelo(cep);
+
+        System.out.println("Logradouro: " + endereco.getLogradouro());
+        System.out.println("Bairro: " + endereco.getBairro());
+        System.out.println("Localidade: " + endereco.getLocalidade());
+
+    }
+}
